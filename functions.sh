4ae1fe6639bc1d61fe5531b200a3f54d927e2eea
Pushkraj Jori <pushkrajjori19@gmail.com>
2025-05-10 13:39:35 +0530

    Create functions.sh

---
+++ functions.sh
@@ -0,0 +1,15 @@
+# php‑toolkit/functions.sh
+# shellcheck shell=bash
+set -euo pipefail
+
+########################################
+# phpinstall  …  (paste from earlier)
+########################################
+phpinstall() { … }
+
+########################################
+# phpswitch  …  (paste the polished version)
+########################################
+phpswitch() { … }
+
+# add more helpers below …
